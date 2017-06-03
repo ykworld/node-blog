d8bcc241cc0c81d0fa18a435ea3a466363a44d7d
ykworld <yonseung.kim@gmail.com>
2017-06-03 19:48:53 +0000

    bug fix

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = require('express')()
+var app = require('express')();
 
 var posts = [
   {
@@ -25,26 +25,26 @@ var posts = [
     title: 'Events',
     body: 'Blog post number 4'
   }
-]
+];
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('home', { posts: posts })
-})
+  res.render('home', { posts: posts });
+});
 
 app.get('/post/:id', (req, res) => {
   var post = posts.filter((post) => {
     return post.id == req.params.id
-  })[0]
+  })[0];
 
   res.render('post', {
     author: post.author,
     title: post.title,
     body: post.body
-  })
-})
+  });
+});
 
-app.listen(8080)
+app.listen(8080);
 
-console.log('listening on port 8080')ss
+console.log('listening on port 8080');
